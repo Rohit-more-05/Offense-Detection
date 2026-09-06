4e5ce328037eb34b6b61a17e6d38906a58ca6ffd
Rohit-More28 <morerohit338@gmail.com>
2026-09-06 19:13:45 +0530

    remove unnecessary documentation and temp files from repo

--- check.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import re
-
-html = urllib.request.urlopen('https://memeguard-frontend.onrender.com/assets/index-BzAnB7pe.js').read().decode('utf-8')
-urls = set(re.findall(r'https?://[^\'\"\)\]]+', html))
-for u in urls:
-    if 'memeguard' in u or 'localhost' in u:
-        print(u)
